d21c82c6b58cc7679bb6b7c773d835c99e8a9bd8
Yean <kjan0929@naver.com>
2024-02-05 23:32:08 +0900

    Modify function add

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,10 @@
 const defaultResult = 0;
 let currentResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, ''); // 결과 출력
 }
 
-currentResult = add(1, 2);
+addBtn.addEventListener('click', add); 
 
-let calculationDescription = `(${currentResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
