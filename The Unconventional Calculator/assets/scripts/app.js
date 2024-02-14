a8d43117bad3734067acec0e7bcaf890d20d1b80
Yean <kjan0929@naver.com>
2024-02-14 22:12:23 +0900

    Add falsy & truthy value

--- The Unconventional Calculator/assets/scripts/app.js
+++ The Unconventional Calculator/assets/scripts/app.js
@@ -28,11 +28,13 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
+  const enterdNumber = getUserNumberInput();
   if (
-    calculationType !== "ADD" &&
-    calculationType !== "SUBTRACT" &&
-    calculationType !== "MULTIPLY" &&
-    calculationType !== "DIVIDE"
+    (calculationType !== "ADD" &&
+      calculationType !== "SUBTRACT" &&
+      calculationType !== "MULTIPLY" &&
+      calculationType !== "DIVIDE") ||
+    !enterdNumber
   ) {
     return;
   }
@@ -43,7 +45,6 @@ function calculateResult(calculationType) {
     calculationType === "MULTIPLY" ||
     calculationType === "DIVIDE"
   ) {
-    const enterdNumber = getUserNumberInput();
     const initialResult = currentResult;
     let mathOperator;
     if (calculationType === "ADD") {
