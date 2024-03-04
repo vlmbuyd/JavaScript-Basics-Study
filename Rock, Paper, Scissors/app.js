785bc4396760cc6a7b94a5fbdae2eda84dbc4855
Yean <kjan0929@naver.com>
2024-03-04 21:45:31 +0900

    First commit

---
+++ Rock, Paper, Scissors/app.js
@@ -0,0 +1,7 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const start = function () {
+  console.log("Game is starting...");
+};
+
+startGameBtn.addEventListener("click", start);
