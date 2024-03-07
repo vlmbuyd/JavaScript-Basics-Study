a6a19b0e9a287b2cfda2c96117b869684477fea6
Yean <kjan0929@naver.com>
2024-03-07 16:05:26 +0900

    To print a message to a user

--- Rock, Paper, Scissors/app.js
+++ Rock, Paper, Scissors/app.js
@@ -18,7 +18,7 @@ const getPlayerChoice = () => {
 
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-    return DEFAULT_USER_CHOICE;
+    return;
   }
   return selection;
 };
@@ -64,5 +64,15 @@ startGameBtn.addEventListener("click", () => {
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
   const winner = getWinner(computerChoice, playerChoice);
-  console.log(winner);
+
+  let message = `You picked ${playerChoice}, computer picked ${computerChoice}, therfore you `;
+  if (winner === RESULT_DRAW) {
+    message = message + "had a draw.";
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + "won.";
+  } else {
+    message = message + "lost.";
+  }
+  alert(message);
+  gameIsRunning = false;
 });
