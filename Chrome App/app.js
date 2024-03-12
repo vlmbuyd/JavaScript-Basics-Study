b12f74d712e5fc91ce0125b48cba9abfe8913d7a
Yean <kjan0929@naver.com>
2024-03-12 17:32:28 +0900

    Save user name and output to login screen

--- Chrome App/app.js
+++ Chrome App/app.js
@@ -1,9 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault(); //브라우저의 가본동작(새로고침) 막아줌
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
+  greeeting.innerText = `Hello ${username}`;
+  greeeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit); //submit === click or enter
