84e13eb78015da0f603328521df4553d2c0cd5bb
Yean <kjan0929@naver.com>
2024-03-12 21:47:13 +0900

    Loading username

--- Chrome App/app.js
+++ Chrome App/app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault(); //브라우저의 가본동작(새로고침) 막아줌
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetngs(username);
+}
+
+function paintGreetngs(username) {
   greeeting.innerText = `Hello ${username}`;
   greeeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit); //submit === click or enter
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME); //로그인 화면 보이게
+  loginForm.addEventListener("submit", onLoginSubmit); //submit === click or enter
+} else {
+  //localstorge에 username이 있을 때
+  paintGreetngs(savedUsername);
+}
