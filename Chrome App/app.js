aacd0549a79c20c58b6d8eecf76eef1ea4d4279b
Yean <kjan0929@naver.com>
2024-03-12 21:10:14 +0900

    Saving username

--- Chrome App/app.js
+++ Chrome App/app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
   event.preventDefault(); //브라우저의 가본동작(새로고침) 막아줌
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem("username", username);
   greeeting.innerText = `Hello ${username}`;
   greeeting.classList.remove(HIDDEN_CLASSNAME);
 }
