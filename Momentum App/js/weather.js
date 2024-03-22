c38e5921de76a5b7f189e062b9f80effa65aba62
Yean <kjan0929@naver.com>
2024-03-22 19:43:06 +0900

    Display on the weather screen based on the user's geographic location

--- Momentum App/js/weather.js
+++ Momentum App/js/weather.js
@@ -1,11 +1,19 @@
 const API_KEY = "5377fb2226b74b0e38b9542173c7f1da";
 
 function onGeoOk(position) {
+  console.log(position);
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   console.log("You live in", lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url).then(response => );
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoError() {
