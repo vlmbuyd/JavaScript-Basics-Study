4da5494d661575e9fca24b5275756f8b697ff0ca
Yean <kjan0929@naver.com>
2024-05-12 22:41:14 +0900

    Mark completion when check todo

--- To do list/app.js
+++ To do list/app.js
@@ -1,40 +1,55 @@
-const form = document.getElementById("todo-form");
+const form = document.querySelector("form");
 const input = form.querySelector("input");
 const btn = document.getElementById("btn");
-const todo = document.querySelector(".todo");
 const ul = document.querySelector("ul");
 
-const checkClick = (i) => {
-  if (
-    ul.children[i].querySelector("li").style.textDecorationLine ===
-    "line-through"
-  ) {
-    ul.children[i].querySelector("li").style.textDecorationLine = "";
-  } else {
-    ul.children[i].querySelector("li").style.textDecorationLine =
-      "line-through";
+let toDos = [];
+
+function checkClick(span, id) {
+  for (const el of toDos) {
+    if (Number(id) === el.id) {
+      if (el.isDone === false) {
+        el.isDone = true;
+        span.classList.toggle("underline");
+      } else {
+        el.isDone = false;
+        span.classList.toggle("underline");
+      }
+    }
   }
-};
+}
 
 function paintToDo(newToDo) {
-  let li = document.createElement("li");
+  const li = document.createElement("li");
   ul.appendChild(li);
-  li.innerText = newToDo;
-  let check = document.createElement("input");
+  li.id = newToDo.id;
+
+  const check = document.createElement("input");
   check.setAttribute("type", "checkbox");
   li.appendChild(check);
-  let del = document.createElement("span");
-  li.appendChild(del);
-  del.innerText = "❌";
 
-  check.addEventListener("click", checkClick);
+  const span = document.createElement("span");
+  span.innerText = newToDo.text;
+  li.appendChild(span);
+
+  const del = document.createElement("span");
+  del.innerText = "❌";
+  li.appendChild(del);
+
+  check.addEventListener("click", () => checkClick(span, li.id));
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const newToDo = input.value;
   input.value = "";
-  paintToDo(newToDo);
+  const newToDoObj = {
+    id: Date.now(),
+    text: newToDo,
+    isDone: false,
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
 }
 
 form.addEventListener("submit", handleSubmit);
